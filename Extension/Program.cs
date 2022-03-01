40948680de60daa5bc5c56530fe7ab24fcc1e691
Ismayil9918 <ismayilbi@code.edu.az>
2022-03-01 07:38:03 +0400

    Homework

---
+++ Extension/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Extension
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "Extension Classi Static Olmalidir";
+            Console.WriteLine(name.Contains("Classi"));
+        }
+    }
+}
